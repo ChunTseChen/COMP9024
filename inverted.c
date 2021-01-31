a759ea158ca2d5ab6137afc3c96dfd7fe6d3f761
Chris Kao <chris.kao@cadmen.com>
2021-01-31 18:49:55 +0800

    Finally done

--- inverted.c
+++ inverted.c
@@ -1,25 +1,25 @@
-#include "DataOperate.h"
-#include <ctype.h>
+// #include "DataOperate.h"
+// #include <ctype.h>
 
-int main(void) {
+// int main(void) {
 
-  // 1. Read file "collection.txt" and add into the list
+//   // 1. Read file "collection.txt" and add into the list
 
-  // create new list
-  DLListStr dataUrls = newDLListStr();
+//   // create new list
+//   DLListStr dataUrls = newDLListStr();
 
-  // read and save the data to the list
-  getCollection(dataUrls);
+//   // read and save the data to the list
+//   getCollection(dataUrls);
 
-  // 2.Read the datas from section-2 and input to the list
-  Tree tree = readSection2toTree(dataUrls);
+//   // 2.Read the datas from section-2 and input to the list
+//   Tree tree = readSection2toTree(dataUrls);
 
-  // 5.Output to the file by order
-  exportInvertedIndex(tree);
+//   // 5.Output to the file by order
+//   exportInvertedIndex(tree);
 
-  // 6.Free the space
-  freeDLListStr(dataUrls);
-  freeTree(tree);
+//   // 6.Free the space
+//   freeDLListStr(dataUrls);
+//   freeTree(tree);
 
-  return 0;
-}
+//   return 0;
+// }
